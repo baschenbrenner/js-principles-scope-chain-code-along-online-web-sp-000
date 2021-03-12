a457709c0755208b8aec0444c84807095f94d508
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2021-03-12 02:27:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /* Your code goes here */
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer(){
+   bestCustomer ="not bob"
+  
+  return bestCustomer
+}
+
+function overWriteBestCustomer(){
+  bestCustomer="maybe bob"
+  return bestCustomer
+}
+
+const leastFavoriteCustomer = "ben"
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Jim"
+}
+
+
+function attemptTwoFavoriteCustomers(){
+  
+}
